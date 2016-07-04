8dd4ac926971f60a1225862f2279dbb40eddba56
Nemanja Boric <4burgos@gmail.com>
2016-07-04 20:43:05 +0200

    Add ELF header definition
    This commits add struct for ELF32 header, as found
    in FreeBSD man pages.

--- src/elf.rs
+++ src/elf.rs
@@ -17,3 +17,23 @@ type Elf32_Sword = i32;
 #[allow(non_camel_case_types)]
 type Elf32_Word = u32;
 
+/// Elf32 header.
+/// Source: http://www.freebsd.org/cgi/man.cgi?elf(5)
+#[repr(C)]
+pub struct Elf32_Ehdr {
+    e_ident: [u8; EI_NIDENT],
+    e_type: Elf32_Half,
+    e_machine: Elf32_Half,
+    e_version: Elf32_Word,
+    e_entry: Elf32_Addr,
+    e_phoff: Elf32_Off,
+    e_shoff: Elf32_Off,
+    e_flags: Elf32_Word,
+    e_ehsize: Elf32_Half,
+    e_phentsize: Elf32_Half,
+    e_phnum: Elf32_Half,
+    e_shentsize: Elf32_Half,
+    e_shnum: Elf32_Half,
+    e_shstrndx: Elf32_Half,
+}
+
